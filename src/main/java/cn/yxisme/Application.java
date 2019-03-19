db0c2f696efaf1ac7ec80116a8a8f978df4347fb
yxisme <yxisme@foxmail.com>
2019-03-20 01:02:26 +0800

    更换Java不建议使用的API

--- src/main/java/cn/yxisme/Application.java
+++ src/main/java/cn/yxisme/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 
 import javax.servlet.MultipartConfigElement;
 
@@ -21,10 +22,11 @@ public class Application {
 	@Bean
 	public MultipartConfigElement multipartConfigElement() {
 		MultipartConfigFactory factory = new MultipartConfigFactory();
-		//单个文件最大
-		factory.setMaxFileSize("10MB"); //KB,MB
-		/// 设置总上传数据总大小
-		factory.setMaxRequestSize("10MB");
+
+		// 单个文件最大
+		factory.setMaxFileSize(DataSize.ofMegabytes(10));
+		// 设置总上传数据总大小
+		factory.setMaxRequestSize(DataSize.ofMegabytes(10));
 		return factory.createMultipartConfig();
 	}
 }
